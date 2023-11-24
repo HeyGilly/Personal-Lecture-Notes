fec5668f45011e4a83aff9938babe4e49f2e3d47
heygilly <nick.gilmore20@gmail.com>
2023-11-24 00:48:14 -0600

    built an example for binary search

--- src/grokking_algoithms/Main.java
+++ src/grokking_algoithms/Main.java
@@ -1,9 +1,39 @@
 package grokking_algoithms;
 
 public class Main {
-    public static void main(String[] args) {
 
-        System.out.println("Hey");
+    public static Integer binarySearch(int[] list, int item) {
+        int low = 0;
+        int high = list.length - 1;
 
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int guess = list[mid];
+
+            if (guess == item) {
+                return mid;
+            }
+
+            if (guess > item) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return null;
     }
-}
+
+
+    public static void main(String[] args) {
+        int[] myList = {1, 3, 5, 7, 9};
+        int searchItem = 3;
+
+        Integer result = binarySearch(myList, searchItem);
+
+        if (result != null) {
+            System.out.println("Item found at index: " + result);
+        } else {
+            System.out.println("Item not found in the list.");
+        }
+}}
